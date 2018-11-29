f260b0d30531d16c5bcd8f115ae1280db9e90999
孔NERD <smallnerd.k@gmail.com>
2018-11-29 15:43:02 +0800

    Add download licenses script add license file

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# mkdir license 
+
+curl -o ./license/GPL-v3 http://www.gnu.org/licenses/gpl.txt 
+curl -o ./license/GPL-v2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt 
+curl -o ./license/GPL-v1 http://www.gnu.org/licenses/old-licenses/gpl-1.0.txt
+curl -o ./license/LGPL-v3 http://www.gnu.org/licenses/lgpl.txt 
+curl -o ./license/LGPL-v2_1 http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+curl -o ./license/LGPL-v2_0 http://www.gnu.org/licenses/old-licenses/lgpl-2.0.txt 
+curl -o ./license/AGPL-v3 http://www.gnu.org/licenses/agpl.txt 
+curl -o ./license/FDL-v1_3 http://www.gnu.org/licenses/fdl-1.3.txt 
+curl -o ./license/FDL-v1_2 http://www.gnu.org/licenses/old-licenses/fdl-1.2.txt
+curl -o ./license/FDL-v1_1 http://www.gnu.org/licenses/old-licenses/fdl-1.1.txt 
+curl -o ./license/MPL-v2_0 https://www.mozilla.org/media/MPL/2.0/index.815ca599c9df.txt
+curl -o ./license/MPL-v1_1 https://www.mozilla.org/media/MPL/1.1/index.0c5913925d40.txt 
+curl -o ./license/AL-v2 http://www.apache.org/licenses/LICENSE-2.0.txt 
+curl -o ./license/AL-v1_1 http://www.apache.org/licenses/LICENSE-1.1 
+curl -o ./license/AL-v1_0 http://www.apache.org/licenses/LICENSE-1.0 
+
